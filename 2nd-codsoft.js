ddf4075ed8d3d48da382d734ead72126f5258262
amish12822 <amishofficial23@gmail.com>
2024-06-08 15:52:09 +0530

    complete 2ns task codsoft

---
+++ 2nd-codsoft.js
@@ -0,0 +1,106 @@
+const quizData = [
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Cascading Style Sheet",
+        c: "Jason Object Notation",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Jason Object Notation",
+        b: "Cascading Style Sheet",
+        c: "Helicopters Terminals Motorboats Lamborginis",
+        d: "Hypertext Markup Language",
+        correct: "b",
+    },
+    {
+        question: "What is HTML syntax?",
+        a: ".html",
+        b: ".js",
+        c: ".css",
+        d: "none of the above",
+        correct: "a",
+    },
+    {
+        question: "What is CSS syntax?",
+        a: ".html",
+        b: ".css",
+        c: ".js",
+        d: "none of the above",
+        correct: "b",
+    },
+    {
+        question: "What is JavaScript syntax?",
+        a: ".css",
+        b: ".html",
+        c: ".js",
+        d: "none of the above",
+        correct: "c",
+    }
+    ];
+    let index = 0;
+    let correct = 0,
+    incorrect = 0,
+    total = quizData.length;
+    let questionBox = document.getElementById("questionBox");
+    let allInputs = document.querySelectorAll("input[type='radio']")
+    const loadQuestion = () => {
+    if (total === index) {
+        return quizEnd()
+    }
+    reset()
+    const data = quizData[index]
+    questionBox.innerHTML = `${index + 1}) ${data.question}`
+    allInputs[0].nextElementSibling.innerText = data.a
+    allInputs[1].nextElementSibling.innerText = data.b
+    allInputs[2].nextElementSibling.innerText = data.c
+    allInputs[3].nextElementSibling.innerText = data.d
+    }
+    
+    document.querySelector("#submit").addEventListener(
+    "click",
+    function() {
+        const data = quizData[index]
+        const ans = getAnswer()
+        if (ans === data.correct) {
+            correct++;
+        } else {
+            incorrect++;
+        }
+        index++;
+        loadQuestion()
+    }
+    )
+    
+    const getAnswer = () => {
+    let ans;
+    allInputs.forEach(
+        (inputEl) => {
+            if (inputEl.checked) {
+                ans = inputEl.value;
+            }
+        }
+    )
+    return ans;
+    }
+    
+    const reset = () => {
+    allInputs.forEach(
+        (inputEl) => {
+            inputEl.checked = false;
+        }
+    )
+    }
+    
+    const quizEnd = () => {
+    // console.log(document.getElementsByClassName("container"));
+    document.getElementsByClassName("container")[0].innerHTML = `
+        <div class="co">
+            <h3 class="w-100"> Hii, you've scored ${correct} / ${total} </h3>
+        </div>
+    `
+    }
+    loadQuestion(index);
+    
